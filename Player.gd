c853fb6edf9a331bb72d8f557fcd964c5b504526
Will Cipriano <will@willcipriano.com>
2021-10-01 18:59:09 -0400

    Part 2

--- Player.gd
+++ Player.gd
@@ -1,7 +1,14 @@
 extends KinematicBody2D
 
+var bullet : PackedScene = preload("res://Player_bullet.tscn")
+
+signal create_bullet(bullet, location)
+	
+
 func _process(delta):
 	
 	var mouse_pos : Vector2 = get_viewport().get_mouse_position()
 	global_position.y = lerp(global_position.y, mouse_pos.y, 0.2)
-	global_position.x = lerp(global_position.x, mouse_pos.x, 0.2)
+	
+	if Input.is_action_just_pressed("shoot"):
+		emit_signal("create_bullet", bullet, global_position)
